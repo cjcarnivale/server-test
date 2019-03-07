869960dd673f457260ea6f2dc479d9bf5b119948
Christopher <christopher.carnivale@gmail.com>
2019-03-07 08:08:07 -0500

    Working post and get routes

---
+++ test-server.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const express = require('express'); 
+
+const app = express();
+const bodyParser = express.json();  
+
+let data = {name: 'Chris'}; 
+
+app.get('/', (req, res, next) => {
+  res.status(200).json(data).end(); 
+}); 
+
+app.post('/', bodyParser, (req, res, next) => { 
+  data = {...data, name: req.body.name};
+  res.status(201).json(req.body.name).end();  
+});
+
+app.listen(8000, () => console.log('Server listening on port 8000'));  
