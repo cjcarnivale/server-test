b673a32751a16b8993c6cf5cbed37ffd62704f28
Christopher <christopher.carnivale@gmail.com>
2019-03-08 16:54:23 -0500

    fixed tests

--- test-server.js
+++ test-server.js
@@ -13,8 +13,9 @@ app.get('/', (req, res, next) => {
 
 app.post('/', bodyParser, (req, res, next) => { 
   data.push({name: req.body.name}); 
-  res.status(201).json(req.body.name).end();  
+  res.status(201).json({name: req.body.name});  
 });
 
 app.listen(8000, () => console.log('Server listening on port 8000'));  
 
+module.exports = {app}; 
--- test/test-server.spec.js
+++ test/test-server.spec.js
@@ -5,17 +5,17 @@ const supertest = require('supertest');
 const express = require('express');
  
 
-const app = express(); 
+const { app }= require('../test-server');
 
 describe('Endpoint Functionality', () => {
   it('Expect 200 on GET request to / endpoint', () => {
-    supertest(app)
+    return supertest(app)
       .get('/')
-      .expect(200, {name: 'Chris'}); 
+      .expect(200, [ {name: 'Chris'} ]); 
   }); 
 
   it('Expect 201 on valid POST request to / endpoint', () => {
-    supertest(app)
+    return supertest(app)
       .post('/')
       .send( {name: 'Shaq'})
       .expect(201, {name: 'Shaq'});  
